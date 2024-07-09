2d1d417e0f0c98b86204fcf5b5c0ab7e6d747b3c
maofu <2148334528@qq.com>
2024-07-09 10:48:52 +0800

    #FEAT: 实现井字棋模块的点击下棋

--- src/components/TicTacToe/TicTacToe.tsx
+++ src/components/TicTacToe/TicTacToe.tsx
@@ -1,17 +1,52 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Board from '../Board/Board' // 导入棋盘组件
 import type { SquaresList } from '../Board/Board' // 导入棋盘格子列表的类型定义
+
+/** 计算井字棋的胜利者 */
+const calculateWinner = function calculateWinner(squares: SquaresList) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
+}
+
+/** 获取初始棋盘数据 */
+const getInitSquares = function getInitSquares(): SquaresList {
+  return Array(9).fill(null)
+}
 
 /** 井字棋游戏组件 */
 const TicTacToe: React.FC = () => {
   /** 棋盘状态，初始化为9个null，表示所有格子都是空的 */
-  const [squares, setSquares] = useState<SquaresList>(Array(9).fill(null))
+  const [squares, setSquares] = useState<SquaresList>(getInitSquares())
 
   /** 棋手状态，包含棋手列表和当前棋手的索引 */
   const [playerFlag, setPlayerFlag] = useState({
     list: ['X', 'O'],
     next: 0
   })
+
+  /** 监听棋盘的改变 */
+  useEffect(() => {
+    // 下完一棋后需要判断是否存在胜利者
+    const winner = calculateWinner(squares)
+    if (winner) {
+      window.confirm(`棋手${winner}胜利了！`)
+    }
+  }, [squares])
 
   /**
    * 处理下棋的函数
